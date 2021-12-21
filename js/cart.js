15a7971c62bd024ad81add502aad1a68910ca05f
ANDR <54762489+Andr2002@users.noreply.github.com>
2021-12-21 13:18:03 +0500

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -1,52 +1,62 @@
-let buttons = document.querySelectorAll('.buy-btn');
+function toggleCartStatus() {
+    if (document.querySelector('.modal').children.length == 3)
+        cartWin.querySelector('.cart-status').innerText = 'Корзина пуста';
+    else {
+        cartWin.querySelector('.cart-status').innerText = '';
+    }
+    console.log(document.querySelector('.modal').children.length);
+}
+
+const cartWin = document.querySelector('.modal');
+
+const buttons = document.querySelectorAll('.buy-btn');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        let id = button.dataset.id;
-        let parent = button.closest('.card');
-        let name = parent.querySelector('.card-name');
-        let price = parent.querySelector('.price');
-        //#region Картинка
-        // let img = parent.getElementsByTagName('img');
-        // var win = window.open('/', 'example', 'width=400,height=400');
-        // win.document.write(`ID товара - ${id}
-        // <br>Наименование товара - ${name.textContent}
-        // <br>Цена - ${parseInt(price.textContent)}
-        // <br><img src="${img[0].currentSrc}" class="card-img">
-        // <style>
-        // .card-img{
-        //     width: 300;
-        //     height: 340px;
-        //     object-fit: contain;
-        // }
-        // </style>`);
-        //#endregion
-        console.log(`ID товара - ${id}\nНаименование товара - ${name.textContent}\nЦена - ${parseInt(price.textContent)}`);
+        let itemId = button.dataset.id;
+        let card = button.closest('.card');
+        let itemTitle = card.querySelector('.card-name');
+        let itemPrice = card.querySelector('.price');
+        let item = {
+            id: itemId,
+            title: itemTitle.textContent,
+            price: parseInt(itemPrice.textContent),
+            imgSrc: card.querySelector('.card-img').getAttribute('src')
+        };
+
+        const itemInCart = cartWin.querySelector(`[data-id="${item.id}"]`);
+        let finalPrice = 0;
+
+        //  если товар в корзине
+        if (itemInCart) {
+            const itemsCount = itemInCart.querySelector('.item-count');
+            itemsCount.innerText = parseInt(itemsCount.textContent) + 1;
+
+            //  удаление из корзины
+            document.querySelector('.delete').addEventListener('click', () => {
+                cartWin.querySelector(`[data-id="${item.id}"]`).remove();
+                toggleCartStatus();
+                // document.querySelector('.cart-content').remove();
+            });
+        } else {
+            // const cartStatus = cartWin.querySelector('.cart-status');
+            // cartStatus.innerText = '';
+            const cartItemHTML = `        
+            <div class="cart-content" data-id="${item.id}">
+            <img src="${item.imgSrc}" alt="${item.title}" class="cart-img">
+            <p class="cart-title">${item.title}</p>
+            <p>Количество: <span class="item-count">1</span> шт.</p>
+            <p>Стоимость: <span class="cart-price">${item.price}</span> ₽</p>
+            <a href="#" class="delete"><img class="delete-logo" src="img/delete-from-cart.png" alt="delete-logo"></a>
+            </div>`;
+            cartWin.insertAdjacentHTML('afterbegin', cartItemHTML);
+            toggleCartStatus();
+        }
+
+        //  удаление из корзины
+        document.querySelector('.delete').addEventListener('click', () => {
+            cartWin.querySelector(`[data-id="${item.id}"]`).remove();
+            toggleCartStatus();
+            // document.querySelector('.cart-content').remove();
+        });
     });
 });
-/*
-let btn = document.querySelector('[data-id="rf-1"]');
-let item = document.querySelector('.card-name');
-let price = document.querySelector('.price');
-
-btn.addEventListener('click', () => {
-    // console.log('click');
-    item.innerText = 'change';
-});
-// let item = document.querySelector('.card-name').textContent;
-// let price = document.querySelector('.price').textContent.substring(0, 4);
-// console.log(Number(price));
-
-console.log(`Товар - ${item.textContent}\nЦена - ${parseInt(price.textContent.substring(0, 4))}`);
-
-
-window.addEventListener('click', (event) => {
-    for (let i = 0; i < document.querySelector('.cards').children.length; i++) { //  document.querySelector('.cards').children.length - получаем количество дочерних элементов
-        if (event.target.dataset.id === `rf-${i+1}`) {
-            const el = event.target.closest('.card'); //  нашли родителя
-            let itemName = el.querySelector('.card-name');
-            let itemPrice = el.querySelector('.price');
-            console.log(`${itemName.textContent}\n${itemPrice.textContent.slice(0, -2)}`);
-        }
-    }
-});
-*/
